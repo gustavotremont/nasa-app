ebf8f19b60668704f068e183b5241ae6071ce7b0
Gustavo Tremont <gustavotremontsr@gmail.com>
2021-12-28 00:02:21 +0100

    create mongo db

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+/****************** Nodejs Dependencies ******************/
 const express = require('express');
-
 require('dotenv').config();
 
+/****************** database connection ******************/
+require('./utils/dbMongo');
+
+/****************** Enable Express ******************/
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded( { extended: false } ));
 // app.use(cors()); //Inhabilita el error de CORS
 
+/****************** Actice Server ******************/
 app.listen(port, () => {
     console.log(`ServerOn http://localhost:${port}`)
 });
